385804c4f4207a559df75593f5d4224458b51422
Nikolai Yakuschenko <nikolai.yakuschenko@gmail.com>
2018-04-24 23:47:51 +0300

    List of users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,91 @@
-import { Component } from '@angular/core';
-
-class Item {
-  purchase: string;
-  done: boolean;
-  price: number;
-
-  constructor(purchase: string, price: number) {
-
-    this.purchase = purchase;
-    this.price = price;
-    this.done = false;
-  }
-}
+import {TemplateRef, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from './user';
+import {UserService} from './user.service';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  providers: [UserService]
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  items: Item[] =
-    [
-      {purchase: "Bread", done: false, price: 15.9},
-      {purchase: "Butter", done: false, price: 60},
-      {purchase: "Potatoes", done: true, price: 22.6},
-      {purchase: "Cheese", done: false, price:310}
-    ];
-  addItem(text: string, price: number): void {
 
-    if(text === null || text.trim() === "" || price === null) {
-      return;
+export class AppComponent implements OnInit{
+  @ViewChild('readOnlyTemplate') readOnlyTemplate: TemplateRef<any>;
+  @ViewChild('editTemplate') editTemplate: TemplateRef<any>;
+
+  editedUser: User;
+  users: Array<User>;
+  isNewRecord: boolean;
+  statusMessage: string;
+
+  constructor(private serv: UserService) {
+    this.users = new Array<User>();
+  }
+
+  ngOnInit() {
+    this.loadUsers();
+  }
+
+  private loadUsers() {
+    this.serv.getUsers().subscribe((data: User[]) => {
+      this.users = data;
+    });
+  }
+
+  addUser() {
+    this.editedUser = new User(0,"",0);
+    this.users.push(this.editedUser);
+    this.isNewRecord = true;
+  }
+
+
+  editUser(user: User) {
+    this.editedUser = new User(user.Id, user.Name, user.Age);
+  }
+
+  loadTemplate(user: User) {
+    if (this.editedUser && this.editedUser.Id == user.Id) {
+      return this.editTemplate;
+    } else {
+      return this.readOnlyTemplate;
     }
-    this.items.push(new Item(text, price));
+  }
+
+  saveUser() {
+    if (this.isNewRecord) {
+
+      this.serv.createUser(this.editedUser).subscribe(data => {
+        this.statusMessage = 'Данные успешно добавлены',
+          this.loadUsers();
+      });
+      this.isNewRecord = false;
+      this.editedUser = null;
+    } else {
+
+      this.serv.updateUser(this.editedUser.Id, this.editedUser).subscribe(data => {
+        this.statusMessage = 'Данные успешно обновлены',
+          this.loadUsers();
+      });
+      this.editedUser = null;
+    }
+  }
+
+  cancel() {
+
+    if (this.isNewRecord) {
+      this.users.pop();
+      this.isNewRecord = false;
+    }
+    this.editedUser = null;
+  }
+
+  deleteUser(user: User) {
+    this.serv.deleteUser(user.Id).subscribe(data => {
+      this.statusMessage = 'Данные успешно удалены',
+        this.loadUsers();
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule }   from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -12,9 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  // providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
